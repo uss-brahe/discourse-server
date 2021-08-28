23eda0e6518b15638c5e9ed12ad7ef883f6c0db8
Zaliel Sel <zaliel.sel@outlook.com>
2021-08-28 19:22:43 -0400

    require script to run under root

--- post-cloudformation.sh
+++ post-cloudformation.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo apt install docker git -y
-sudo mkdir /mnt/xvdh
-sudo mkfs -t ext4 /dev/nvme1n1
-sudo sh -c "echo '/dev/nvme1n1       /mnt/xvdh   ext4    defaults,nofail 0 2' >> /etc/fstab"
-sudo mount -a
-sudo git clone https://github.com/discourse/discourse_docker.git /mnt/xvdh/discourse
+if [ $EUID -ne 0 ]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+apt install docker git -y
+mkdir /mnt/xvdh
+mkfs -t ext4 /dev/nvme1n1
+echo '/dev/nvme1n1       /mnt/xvdh   ext4    defaults,nofail 0 2' >> /etc/fstab
+mount -a
+git clone https://github.com/discourse/discourse_docker.git /mnt/xvdh/discourse
