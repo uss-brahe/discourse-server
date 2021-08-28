f33434d8ee2cd1fe19ffae5a21b81216ce7d6938
Zaliel Sel <zaliel.sel@outlook.com>
2021-08-28 19:26:48 -0400

    creation of directories will no longer cause errors if the directory was previously created

--- post-cloudformation.sh
+++ post-cloudformation.sh
@@ -6,7 +6,7 @@ if [ $EUID -ne 0 ]; then
 fi
 
 apt install docker git -y
-mkdir /mnt/xvdh
+mkdir -p /mnt/xvdh
 mkfs -t ext4 /dev/nvme1n1
 echo '/dev/nvme1n1       /mnt/xvdh   ext4    defaults,nofail 0 2' >> /etc/fstab
 mount -a
