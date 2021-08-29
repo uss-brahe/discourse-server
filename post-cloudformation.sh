04f4e7e18ebbefc671fde5a75ee8da0f945a55ff
Zaliel Sel <zaliel.sel@outlook.com>
2021-08-28 22:13:56 -0400

    fixed some path refreneces

--- post-cloudformation.sh
+++ post-cloudformation.sh
@@ -10,10 +10,10 @@ echo '/dev/nvme1n1       /mnt/xvdh   ext4    defaults,nofail 0 2' >> /etc/fstab
 mount -a
 
 # extend the swap space (Discourse requires at least 2 GB)
-/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
-/sbin/mkswap /mnt/xvdhswap.1
-chmod 600 /mnt/xvdhswap.1
-/sbin/swapon /mnt/xvdhswap.1
+/bin/dd if=/dev/zero of=/mnt/xvdh/swap.1 bs=1M count=2048
+/sbin/mkswap /mnt/xvdh/swap.1
+chmod 600 /mnt/xvdh/swap.1
+/sbin/swapon /mnt/xvdh/swap.1
 
 # clone discourse
 git clone https://github.com/discourse/discourse_docker.git /mnt/xvdh/discourse
